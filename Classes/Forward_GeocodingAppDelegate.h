c30ab256c29d94a3e9d02b884601058bb87e641f
bjornsallarp <bjornsallarp@ed7e79ea-3796-a3ba-ef90-f6361be562a0>
2010-11-17 09:53:13 +0000

    git-svn-id: http://bsforwardgeocoder.googlecode.com/svn/trunk@2 ed7e79ea-3796-a3ba-ef90-f6361be562a0

---
+++ Classes/Forward_GeocodingAppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  Created by Björn Sållarp on 2010-03-13.
+//  NO Copyright 2010 MightyLittle Industries. NO rights reserved.
+// 
+//  Use this code any way you like. If you do like it, please
+//  link to my blog and/or write a friendly comment. Thank you!
+//
+//  Read my blog @ http://blog.sallarp.com
+//
+
+
+#import <UIKit/UIKit.h>
+
+@class Forward_GeocodingViewController;
+
+@interface Forward_GeocodingAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    Forward_GeocodingViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet Forward_GeocodingViewController *viewController;
+
+@end
+
